1c3d1b67071bcf2920008205072504c546fb1d0e
Apoorv Vikram Singh <apoorvvikram.singh@iiitb.org>
2014-07-04 19:57:29 +0530

    Update happiest_state.py

--- happiest_state.py
+++ happiest_state.py
@@ -4,6 +4,64 @@ import codecs
 
 def main(): 
     happiness={}
+    #you can add your own states too.
+    states={'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'}
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     scores = {} # initialize an empty dictionary  
@@ -12,10 +70,10 @@ def main():
         scores[term] = int(score) # Convert the score to an integer.  
     for line in tweet_file:
         a = json.loads(line)
-        if 'place' in a and a['place'] is not None and a['place']['country_code']=='US' : # You can choose your country
+        if 'place' in a and a['place'] is not None  : 
             if 'user' in a and a['user'] is not None and a['user']['location'] is not None:
                 location=a['user']['location'].split(' ')
-                if len(location)==2 and len(location[1])==2:
+                if len(location)==2 and len(location[1])==2 and location[1] in states :
                     if 'text' in a:
                         sent_score=0
                         words = a['text'].split()
